e0d2a5856e1f4375a987ec3e3170375087e650b1
tx7do <yanglinbo@gmail.com>
2023-03-04 13:04:04 +0800

    feat: Authenticator add AuthenticateToken function.

--- engine/noop/noop.go
+++ engine/noop/noop.go
@@ -10,14 +10,21 @@ type Authenticator struct{}
 
 var _ engine.Authenticator = (*Authenticator)(nil)
 
-func (n Authenticator) Authenticate(_ context.Context) (*engine.AuthClaims, error) {
+func (n Authenticator) Authenticate(_ context.Context, _ engine.ContextType) (*engine.AuthClaims, error) {
 	return &engine.AuthClaims{
 		Subject: "",
 		Scopes:  nil,
 	}, nil
 }
 
-func (n Authenticator) CreateIdentity(_ context.Context, _ engine.AuthClaims) (string, error) {
+func (n Authenticator) AuthenticateToken(_ string) (*engine.AuthClaims, error) {
+	return &engine.AuthClaims{
+		Subject: "",
+		Scopes:  nil,
+	}, nil
+}
+
+func (n Authenticator) CreateIdentity(_ context.Context, _ engine.ContextType, _ engine.AuthClaims) (string, error) {
 	return "", nil
 }
 
